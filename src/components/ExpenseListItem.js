053ed725c1046c4467ed81655b64560245f469de
amrit chhetri <amritchhetri@gmail.com>
2018-07-24 20:38:53 +0200

    currency format with numeral and date format with moment js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { Link} from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
+// load a locale
+numeral.register('locale', 'no', {
+  delimiters: {
+      thousands: '.',
+      decimal: ','
+  },
+  abbreviations: {
+      thousand: 'k',
+      million: 'm',
+      billion: 'b',
+      trillion: 't'
+  },
+  ordinal : function (number) {
+      return number === 1 ? 'er' : 'ème';
+  },
+  currency: {
+      symbol: 'NOK '
+  }
+});
 
-
+numeral.locale('no');
  const ExpenseListItem=(props)=>(
     <div>
     <h1>Item desc</h1>
   <Link to={`/edit/${props.expenseItem.id}`}> {props.expenseItem.description}</Link>  
     <br/>
-   Amount: {props.expenseItem.amount}
+   Amount: {numeral((props.expenseItem.amount)/100).format("$0,0.00")}
     <br/>
-   Created At: {props.expenseItem.createdAt}
+   Created At: {moment(props.expenseItem.createdAt).format("Do MMMM, YYYY")}
    <br/>
    
     </div>
