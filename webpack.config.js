3089f9e26ed5a5d6dd4e8b1c6462bb845412682b
amrit chhetri <amritchhetri@gmail.com>
2018-07-24 20:07:45 +0200

    added dist folder for serving production

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports=(env)=>{
 return{
   entry: './src/App.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js'
   },
   module: {
@@ -38,10 +38,11 @@ return{
     CSSExtract
   ],
   devtool:isProduction ? "source-map": "inline-source-map",
-  devServer:{
+  devServer:{ //dev server do not write the file in file system, it serve them vurtually
     contentBase:path.join(__dirname, 'public'),
     port:8080,
-    historyApiFallback:true // this tell webserver to send  index.html for all our routes
+    historyApiFallback:true, // this tell webserver to send  index.html for all our routes
+    publicPath:'/dist/'
   }
 };
 
