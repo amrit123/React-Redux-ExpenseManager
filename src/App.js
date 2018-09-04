3f457cbd950c7d89bc249df1595f7cd8f7cd9f7e
amrit chhetri <amritchhetri@gmail.com>
2018-09-05 00:09:30 +0200

    added babel polyfill for older browser support

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 
 firebase.auth().onAuthStateChanged((user) => {
+    
     if (user) {
         store.dispatch(login(user.uid)); //first store user id to the state
         store.dispatch(startSetExpenses()).then(() => {
